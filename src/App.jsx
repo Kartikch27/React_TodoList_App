b073302a5115c32212eb9e61475f1b3e752535c1
karti <kartikchaurasiya2710@gmail.com>
2024-09-21 12:29:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Todolist from './component/Todolist.jsx';
+
+
+function App() {
+  
+
+  return (
+    <>
+     
+     <Todolist />
+    </>
+  ); 
+}
+
+export default App;
